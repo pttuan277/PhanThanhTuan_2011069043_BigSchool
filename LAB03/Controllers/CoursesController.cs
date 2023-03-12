08c9450b80c670425023ba0e46613859ccf3ba97
Admin <Admin@DESKTOP-8PA8D34>
2023-03-12 16:53:42 +0700

    Add project files.

---
+++ LAB03/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using LAB03.Models;
+using LAB03.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB03.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ LAB03/ViewModels/CourseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using LAB03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB03.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+
+
+
+
+    }
+}
